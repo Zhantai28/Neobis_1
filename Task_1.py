ac3ac4bfb7d8657c79d8493abd2ea8e337d1208d
Zhantai <Zhantai.Ismailov@gmail.com>
2021-05-03 11:56:10 +0600

    first commit

---
+++ Task_1.py
@@ -0,0 +1,39 @@
+import json
+
+my_info = {
+    "first name": "Zhantai",
+    "last name": "Ismailov",
+    "age": 20,
+    "Email": "Zhantai.Ismailov@gmail.com",
+    "city": "Bishkek",
+    "university": "Girne American University",
+    "place of work": "Lego robotics teacher",
+    "family": [
+        {
+            "first name": "Islam",
+            "age": 14
+        },
+        {
+            "first name": "Begimay",
+            "age": 18
+        },
+        {
+            "first name": "Tagay",
+            "age": 2
+        }
+    ],
+    "favorites": [
+        {
+            "Favorite Anime": ["Death note", "Naruto", "Attack on Titan"]
+        },
+        {
+            "Favorite Movie": ["The Shawshank redemption", "Knives Out", "Catch Me If You Can"]
+        },
+        {
+            "Favorite dishes": ["Manty", "Samsa", "Shawarma", "Pizza"]
+        }
+    ]
+}
+
+with open("My_info.json", 'w') as write_file:
+    json.dump(my_info, write_file, indent=4)
